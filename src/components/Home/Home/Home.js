0bb67fcdc87856600c1188774dff9d02ee661467
Niloy-Sumon <niloyabidur@gmail.com>
2022-01-01 04:54:00 +0600

    footer done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -4,6 +4,7 @@ import Nav from '../../shared/Nav/Nav'
 import FastFood from '../FastFood/FastFood';
 import CardTable from '../CardTable/CardTable';
 import Shop from '../Shop/Shop';
+import Footer from '../../shared/Footer/Footer/Footer';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <FastFood></FastFood>
             <CardTable></CardTable>
             <Shop></Shop>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/components/shared/Footer/Footer/Footer.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import footerLogo from '../../../../img/logo_svg.svg'
+import payment from '../img/footer_img1.png'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <footer class="text-center text-lg-start bg-dark text-muted text-white">
+                
+                <section class="">
+                    <div class="container text-center text-md-start p-4 border-top mt-5">
+                        <div class="row mt-3">
+                            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    <img src={footerLogo} alt="" />
+                                </h6>
+                                <p>
+                                    Here you can use rows and columns to organize your footer content. Lorem ipsum
+                                    dolor sit amet, consectetur adipisicing elit.
+                                </p>
+                            </div>
+                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    Products
+                                </h6>
+                                <p>
+                                    <a href="#!" class="text-reset">Angular</a>
+                                </p>
+                                <p>
+                                    <a href="#!" class="text-reset">React</a>
+                                </p>
+                                <p>
+                                    <a href="#!" class="text-reset">Vue</a>
+                                </p>
+                                <p>
+                                    <a href="#!" class="text-reset">Laravel</a>
+                                </p>
+                            </div>
+                            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    Useful links
+                                </h6>
+                                <p>
+                                    <a href="#!" class="text-reset">Pricing</a>
+                                </p>
+                                <p>
+                                    <a href="#!" class="text-reset">Settings</a>
+                                </p>
+                                <p>
+                                    <a href="#!" class="text-reset">Orders</a>
+                                </p>
+                                <p>
+                                    <a href="#!" class="text-reset">Help</a>
+                                </p>
+                            </div>
+                            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                                <h6 class="text-uppercase fw-bold mb-4">
+                                    Contact
+                                </h6>
+                                <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+                                <p>
+                                    <i class="fas fa-envelope me-3"></i>
+                                    info@example.com
+                                </p>
+                                <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+                                <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <div className="p-4 footer-copyright" style={{ backgroundColor: '#00a850' }}>
+                    <div className="row">
+                        <div className="col-md-6">
+                            Copyright © 2020 <span style={{color:'#ffc222'}}>pocofood</span>. All Rights Reserved.
+                        </div>
+                        <div className="col-md-6">
+                            <img className='payment-image' src={payment} alt=""  style={{}}/>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
